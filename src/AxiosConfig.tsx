1997ad2eca6e20789ae76f02a2417ea9a89f7e03
gabrielsd02 <xgabrields@gmail.com>
2023-07-17 00:48:55 -0300

    url: update standard url

--- src/AxiosConfig.tsx
+++ src/AxiosConfig.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // define
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://studio-ghibli-finder-service.onrender.com';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
